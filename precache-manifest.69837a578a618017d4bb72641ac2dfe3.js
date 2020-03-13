486f8b787c612c09973535e347d6fb25c4dd718a
Prerna Sharan <prerna.sharan@zensar.com>
2020-03-13 14:40:39 +0530

    Updates

---
+++ precache-manifest.69837a578a618017d4bb72641ac2dfe3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446bccee9d4cbe22b11801d432ca0943",
+    "url": "/Samurai1.0/index.html"
+  },
+  {
+    "revision": "aac3c3d0c62f3a94e80f",
+    "url": "/Samurai1.0/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "2400d8af99708bd62eb4",
+    "url": "/Samurai1.0/static/css/main.2e21a48e.chunk.css"
+  },
+  {
+    "revision": "aac3c3d0c62f3a94e80f",
+    "url": "/Samurai1.0/static/js/2.f66c7b83.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/Samurai1.0/static/js/2.f66c7b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2400d8af99708bd62eb4",
+    "url": "/Samurai1.0/static/js/main.99a31441.chunk.js"
+  },
+  {
+    "revision": "04317683ddf98e737479",
+    "url": "/Samurai1.0/static/js/runtime-main.9a267086.js"
+  },
+  {
+    "revision": "fb6e8c25ecb85b21fa430e7485a36590",
+    "url": "/Samurai1.0/static/media/Icon ionic-ios-exit.fb6e8c25.svg"
+  },
+  {
+    "revision": "be81b13dc55895507ceff3d0d4752cb9",
+    "url": "/Samurai1.0/static/media/Logo.be81b13d.png"
+  }
+]);
